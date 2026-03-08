4bc32d49a695795b196a57157c652ffc8a4ef546
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2026-03-08 11:33:58 +0530

    position

--- length.py
+++ length.py
@@ -6,3 +6,10 @@ counter = 0
 for i in s:
     counter+=1
 print("length of string : ", counter)
+
+
+
+#get user name in the email
+s= input("enter the email")
+position=s.index('@')
+print(s[0:position])
