3f3264c70207b2280869785b9e8135283e111619
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2026-05-22 12:47:46 +0530

    area of the triangle

---
+++ practice_3.py
@@ -0,0 +1,7 @@
+# Find the area of triangle
+base = eval(input("Enter the base of the triangle : "))
+height = eval(input("Enter the height of the triangle : "))
+
+area= 0.5*base*height
+
+print(f" The area of the triangle is : {area}")
