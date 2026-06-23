05f62d0858bb7f3128e6f2845ac34099332c9734
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2026-06-23 14:52:51 +0530

    2) Check Given String is palindrome or not

---
+++ String_by_Ashok_IT/practice_2.py
@@ -0,0 +1,7 @@
+#2) Check Given String is palindrome or not
+String= input("Enter the String:-")
+
+if String== String[::-1]:
+    print(" It is Palindrome", String)
+else:
+    print("It is not palindrome ", String)
