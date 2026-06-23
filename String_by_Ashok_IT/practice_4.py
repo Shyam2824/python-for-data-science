c912ae2f20e25efb6b975eaebe02d842db1d5f3d
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2026-06-23 20:14:37 +0530

    # Count occurence of each character in given string

---
+++ String_by_Ashok_IT/practice_4.py
@@ -0,0 +1,14 @@
+# Count occurence of each character in given string
+
+string= input("Enter the String:- ")
+char_count= {}
+
+for char in string:
+    if char in char_count:
+        char_count[char] +=1
+    else:
+        char_count[char] =1
+    
+print("characteristic :- ")
+#for char, count in char_count.items():
+print(f" {char} : {char_count}") 
