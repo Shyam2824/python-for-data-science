d9709545178cc5630699697e691c9971b6c9afcf
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2026-06-25 11:22:19 +0530

    no of blocks

---
+++ loop ptactice/practice6.py
@@ -0,0 +1,11 @@
+blocks = int(input("Enter number of blocks: "))
+
+height = 0
+layer = 1
+
+while blocks >= layer:
+    blocks -= layer
+    height += 1
+    layer += 1
+
+print("The height of the pyramid:", height)
