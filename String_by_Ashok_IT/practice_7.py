23a5f80f7b8e58734b43c3fd9cb4a8816370c94f
Shyam2824 <97013528+Shyam2824@users.noreply.github.com>
2026-06-26 18:14:57 +0530

    table

---
+++ String_by_Ashok_IT/practice_7.py
@@ -0,0 +1,8 @@
+def f2(a:str, b:str) -> str:
+    return a+b
+
+result = f2(10,20)
+print(result, type(result))
+
+result = f2("Hi","5")
+print(result, type(result))
---
+++ loop ptactice/practice8.py
@@ -0,0 +1,6 @@
+# write table using for loop
+n= int(input("enter the number: "))
+
+
+for i in range(1,11):
+    print(i, "*" , n ," =" , i*n)
